f253aee85dca753b24cece95bee5c3750099c0eb
arun <70808728+itsmekerala@users.noreply.github.com>
2020-09-08 15:10:23 +0300

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,13 +1,19 @@
-a=int(input("enter 1st Number: "))
-b=int(input("enter 2st Number: "))
-c=int(input("enter 3st Number: "))
-if (a>b)and (a>c):
-    print (a,"is the greatest")
-elif (b>a)and(b>c):
-    print (b,"is the greatest")
-elif (a==b):
-    print("1st and 2nd Numbers are same")
-elif(a==c):
-    print("1st and 3nd Numbers are same")
+a=int(input("Enter the First value: "))
+b=int(input("Enter the Second value: "))
+c=int(input("Enter the Third value: "))
+if a>b and a>c:
+    print (a,"is the greatest Value")
+elif b>a and b>c:
+    print(b,"is the greatest  Value")
+elif c>a and c>b:
+    print (c,"is the greatest Value")
+elif a==b:
+    print (a, "and" ,b, "are Equal value's")
+elif a==c:
+    print (a, "and",c," are Equal value's")
+elif b==c:
+    print (b, "and" ,c," are Equal value's ")
+elif b==a:
+    print(b, "and" ,a, "are Equal value's")
 else:
-    print(c,"is the greatest")
+    print ("No Answer")
